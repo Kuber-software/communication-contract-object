93983751134526109a7ec27ab5632d3abdef5033
kulesh-i <Icf?8bj3dGFk>
2021-01-18 18:36:10 +0300

    18012021-1835:add NoSqlManagerMicroservice

--- microservices/NoSqlManagerMicroservice/UserTokenCollection/Interfaces/DeleteUserTokenInterface.php
+++ microservices/NoSqlManagerMicroservice/UserTokenCollection/Interfaces/DeleteUserTokenInterface.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace Kubersoftware\Microservices\NosqlManagerMicroservice\UserTokenCollection\Interfaces;
+
+use Kubersoftware\Microservices\NosqlManagerMicroservice\UserTokenCollection\Dto\ArrayObjectIdDto;
+use MongoDB\BSON\ObjectId;
 
 interface DeleteUserTokenInterface
 {
-    public function delete(string $id): bool;
+    public function deleteOne(ObjectId $id): bool;
+
+    public function deleteMany(ArrayObjectIdDto $arrayObjectIdDto): bool;
 }
--- microservices/NoSqlManagerMicroservice/UserTokenCollection/Interfaces/InsertUserTokenInterface.php
+++ microservices/NoSqlManagerMicroservice/UserTokenCollection/Interfaces/InsertUserTokenInterface.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace Kubersoftware\Microservices\NosqlManagerMicroservice\UserTokenCollection\Interfaces;
 
 use Kubersoftware\Microservices\NosqlManagerMicroservice\UserTokenCollection\Entity\UserTokenEntity;
 
 interface InsertUserTokenInterface
 {
-    public function insert(UserTokenEntity $userTokenEntity): string;
+    public function insertOne(UserTokenEntity $userTokenEntity): string;
+
+    public function insertMany(UserTokenEntity $userTokenEntity): string;
 }
