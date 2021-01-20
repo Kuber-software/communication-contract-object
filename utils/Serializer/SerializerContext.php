b7538ee4e5c63363a18c61cf85cc1876e2604b8f
nezhkind <dnezhkin@gmail.com>
2021-01-20 12:33:36 +0300

    200121-1233: update SerializerContext.php

--- utils/Serializer/SerializerContext.php
+++ utils/Serializer/SerializerContext.php
@@ -13,8 +13,13 @@ class SerializerContext
         $this->serializerTool = $serializerTool;
     }
 
-    public function doSerialize(object $data, string $format): string
+    public function serialize(object $data, string $format): string
     {
         return $this->serializerTool->serialize($data, $format);
+    }
+
+    public function deserialize(string $data, string $objectName, string $format): string
+    {
+        return $this->serializerTool->deserialize($data, $objectName, $format);
     }
 }
